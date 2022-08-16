6f0514ea4aa529e1e2020b466c47fad8a211243e
Valentina Saraeva <kantor-07@mail.ru>
2022-08-16 12:40:06 +0300

    lesson2-hw.v1
    Создала SiteController, в котором прописала маршруты страниц Главной и О нас.
    Создала в папке view папку SitePage и в ней создала представления страниц Главная, О нас, Все новости.
    Все маршруты прописала в web.php

---
+++ lesson2-hw/views/lesson1-hw/topNews.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>topNews</title>
+</head>
+<body> 
+    <ul>
+        <li>
+            <a href="/myNewsSite/public">Главная</a>
+        </li>
+        <li>
+            <a href="/myNewsSite/public/aboutUs">О нас</a>
+        </li>
+        <li>
+            <a href="/myNewsSite/public/topNews">Главные новости</a>
+        </li>
+    </ul>
+    <h1>Главные новости</h1>
+</body>
+</html>
